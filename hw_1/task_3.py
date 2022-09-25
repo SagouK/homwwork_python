80bd5f45fa7bb446a626e79cbcb6f8ffaf84ac44
Sagou <anastasiyazadybinina@gmailcom>
2022-09-25 14:04:09 +0300

    task3

--- hw_1/task_3.py
+++ hw_1/task_3.py
@@ -1,21 +1,22 @@
-def inputNumbers(x):
-    xyz = ["X", "Y", "Z"]
-    a = []
-    for i in range(x):
-        a.append(input(f"Введите значение {xyz[i]}: "))
-    return a
+# Напишите программу, которая принимает на вход координаты точки 
+# (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, 
+# в которой находится эта точка (или на какой оси она находится).
 
-
-def checkPredicate(x):
-    left = not (x[0] or x[1] or x[2])
-    right = not x[0] and not x[1] and not x[2]
-    result = left == right
-    return result
-
-
-statement = inputNumbers(3)
-
-if checkPredicate(statement) == True:
-    print(f"Утверждение истинно")
-else:
-    print(f"Утверждение ложно")
+def quarter_number(x, y):
+    if x > 0 and y > 0:
+        print('1')
+    elif x < 0 and y > 0:
+        print('2')
+    elif x < 0 and y < 0:
+        print('3')
+    else:
+        print('4')
+try:
+    num_1 = int(input('Введите Х: '))
+    num_2 = int(input('Введите Y: '))
+    if num_1 != 0 and num_2 != 0:
+        quarter_number(num_1, num_2)
+    else:
+        print('Координаты должны быть не равны 0!')
+except:
+    print('Ошибка ввода данных!')
